74011aeaf494c218d386419453ca7da88f08e0f9
Chudakov Matvey <morganrush@yandex.ru>
2020-10-19 15:35:18 +0300

    vue-js todo

--- vue-todo/src/components/todo/Todo.vue
+++ vue-todo/src/components/todo/Todo.vue
@@ -4,20 +4,15 @@
       <h4>Список задач</h4>
     </div>
 
-    <div class="todo_new-task">
-      <input type="text" placeholder="Введите текст задачи...">
-      <button class="todo_new-task-button">
-        <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
-          <path d="M24.5 12.25V14H14V24.5H12.25V14H1.75V12.25H12.25V1.75H14V12.25H24.5Z" fill="#CFCACA"/>
-        </svg>
-      </button>
-    </div>
+    <AddTodo v-on:add-todo="createNewTodo($event)"/>
 
     <div class="todo_list">
       <TodoItem
           v-for="todo of todos"
-          v-bind:todo="todo"
-          v-bind:key="todo.id"
+          :todo="todo"
+          :key="todo.id"
+          v-on:check="onCheck(todo)"
+          v-on:remove="onRemove(todo.id)"
       />
     </div>
   </div>
@@ -25,11 +20,13 @@
 
 <script>
 import TodoItem from "@/components/todo/todo-item/TodoItem";
+import AddTodo from "@/components/todo/add-todo/AddTodo";
 
 export default {
   name: "Todo",
   data() {
     return {
+      lastId: 0,
       todos: [
         {
           id: 1,
@@ -59,12 +56,96 @@ export default {
       ]
     }
   },
+  mounted() {
+    this.lastId = Math.max(...this.todos.map(todo => todo.id))
+  },
+  methods: {
+    createNewTodo(description) {
+      this.todos = [
+        {
+          id: ++this.lastId,
+          description,
+          done: false
+        },
+        ...this.todos
+      ]
+    },
+
+    onCheck(todo) {
+      const changedTodo = {
+        ...todo,
+        done: !todo.done
+      }
+      const filteredTodos = this.todos.filter(t => t.id !== todo.id)
+
+      this.todos = [...filteredTodos.filter(t => !t.done), changedTodo, ...filteredTodos.filter(t => t.done)]
+    },
+
+    onRemove(id) {
+      this.todos = this.todos.filter(t => t.id !== id)
+    }
+  },
   components: {
-    TodoItem: TodoItem
+    TodoItem: TodoItem,
+    AddTodo: AddTodo
   }
 }
 </script>
 
-<style scoped>
+<style scoped lang="scss">
+.todo {
+  position: absolute;
+  left: 50%;
+  top: 50%;
+  transform: translate(-50%, -50%);
 
+  width: 520px;
+  height: 550px;
+  background-color: #FFF;
+
+  box-shadow: 0px 4px 4px rgba(0, 0, 0, 0.25);
+  border-radius: 6px;
+  //overflow: auto;
+  border: 1px solid #CCC2C2;
+
+  &_header {
+    background: #4B95EB;
+    padding: 20px;
+
+    h4 {
+      display: flex;
+      justify-content: center;
+      align-items: center;
+
+      color: #FFF;
+      font-size: 26px;
+      font-weight: 500;
+    }
+  }
+
+  &_list {
+    height: calc(550px - 71px - 62px);
+    overflow-y: auto;
+
+    /* Width */
+    &::-webkit-scrollbar {
+      width: 7px;
+    }
+
+    /* Track */
+    &::-webkit-scrollbar-track {
+      background: #f1f1f1;
+    }
+
+    /* Handle */
+    &::-webkit-scrollbar-thumb {
+      background: #83BCFF;
+    }
+
+    /* Handle on hover */
+    &::-webkit-scrollbar-thumb:hover {
+      background: #4B95EB;
+    }
+  }
+}
 </style>
---
+++ vue-todo/src/components/todo/add-todo/AddTodo.vue
@@ -0,0 +1,90 @@
+<template>
+  <div class="todo_new-task">
+    <input type="text" placeholder="Введите текст задачи..." v-model="description">
+    <button class="todo_new-task-button" v-on:click="addTodo()">
+      <svg width="28" height="28" viewBox="0 0 28 28" fill="none" xmlns="http://www.w3.org/2000/svg">
+        <path d="M24.5 12.25V14H14V24.5H12.25V14H1.75V12.25H12.25V1.75H14V12.25H24.5Z" fill="#CFCACA"/>
+      </svg>
+    </button>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "AddTodo",
+  data() {
+    return {
+      description: ''
+    }
+  },
+  methods: {
+    addTodo() {
+      if (this.description) {
+        this.$emit('add-todo', this.description)
+
+        this.description = ''
+      }
+    }
+  }
+}
+</script>
+
+<style scoped lang="scss">
+.todo_new-task {
+  display: flex;
+  height: 62px;
+  border-bottom: 1px solid #D6C8C8;
+
+  input {
+    border: 0;
+    font-size: 20px;
+    padding: 20px;
+    flex: 1;
+
+    &::-webkit-input-placeholder {
+      color: #CFCACA;
+    }
+
+    &:-ms-input-placeholder {
+      color: #CFCACA;
+    }
+
+    &::-ms-input-placeholder {
+      color: #CFCACA;
+    }
+
+    &::placeholder {
+      color: #CFCACA;
+    }
+
+    &:focus {
+      outline: none;
+    }
+  }
+
+  &-button {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    width: 74px;
+
+    border: 0;
+    background-color: transparent;
+
+    &:focus {
+      outline: none;
+    }
+
+    &:hover {
+      background-color: #F6F3F3;
+      cursor: pointer;
+
+      svg {
+        path {
+          fill: #726464;
+        }
+      }
+    }
+  }
+}
+</style>
